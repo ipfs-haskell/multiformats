17f3ba851d6e4e34acf060401af16e358ada0179
adithyaov <111501017@smail.iitpkd.ac.in>
2019-01-15 01:52:44 +0530

    modified multibase

--- multibase/src/MultiBase.hs
+++ multibase/src/MultiBase.hs
@@ -1,6 +1,20 @@
 -- Use signatures available in backpack and paramatrize over monad
 module MultiBase where
 
-class MultiBase a where
-  basePrefix :: a -> String
-  prefixed :: a -> String
+import qualified Data.ByteString as BS
+import Data.Word (Word8)
+
+data MutiBaseFormat = MultiBaseFormat Base Value
+type Base = Word8
+type Value = BS.ByteString
+
+class MultiBaseable a where
+  prefix :: a -> Word8
+
+encode = BS.cons 
+
+decode b = MultiBaseFormat w r
+        where
+          w = BS.head b
+          r = BS.tail b
+
