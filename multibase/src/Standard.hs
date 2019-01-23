48e1a4cd43f30be19fe95652c24cb190eb93def7
adithyaov <111501017@smail.iitpkd.ac.in>
2019-01-23 14:03:25 +0530

    added to base type func

--- multibase/src/Standard.hs
+++ multibase/src/Standard.hs
@@ -10,3 +10,10 @@ instance (Multibaseable a) => Multibaseable (Base a) where
   baseCode Base16 = BS.pack [102]
   baseCode (Other a) = baseCode a
 
+toBaseTypeStandard :: Maybe (BS.ByteString -> Base a) -> BS.ByteString -> Maybe (Base a)
+toBaseTypeStandard mf b
+  | b == BS.pack [50] = Just Base2
+  | b == BS.pack [102] = Just Base16
+  | otherwise = do
+      f <- mf
+      Just $ f b 
